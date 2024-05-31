a641e9d44552469cc9bff58de0005af6d8552e8c
Augusto Rolandelli <84287366+chirogod@users.noreply.github.com>
2024-05-30 23:33:16 -0300

    Add files via upload

--- hash/ejercicio4.c
+++ hash/ejercicio4.c
@@ -3,5 +3,13 @@
 legajo y se guardará como dato la posición física del registro para realizar accesos
 directos. Hacer un ABM para poder cargar manualmente el archivo. El “legajo" es un
 valor de 6 dígitos.
-
+// max de alumnos 100, pero que se pueda cargar menos tmb
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tabla_hash.h" // Suponiendo que tienes definido aquí tu TAD TablaHash
+
+#define MAX_LEGAJO_DIGITOS 6
+#define NOMBRE_ARCHIVO "alumnos.txt"
+#define TAMANIO_HASH 100
